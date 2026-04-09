274aa310e231790aaa650757a5dbcad731060daa
Christopher F A <christopher.armani095@gmail.com>
2026-04-09 16:54:55 +0200

    parameterized topping and order total tests

--- src/test/java/christopherfa/pizza_menu/PizzaMenuApplicationTests.java
+++ src/test/java/christopherfa/pizza_menu/PizzaMenuApplicationTests.java
@@ -2,6 +2,8 @@ package christopherfa.pizza_menu;
 
 import christopherfa.pizza_menu.entities.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -26,4 +28,29 @@ class PizzaMenuApplicationTests {
     void testMargheritaPrice() {
         Pizza margherita = ctx.getBean("pizzaMargherita", Pizza.class);
         assertEquals(4.99, margherita.getPrice(), "La margherita deve costare 4.99€");
-    }}
+    }
+
+    //test parametrico
+    @ParameterizedTest
+    @ValueSource(strings = {"cheese", "ham", "salami"})
+    void testToppingsPriceIsPositive(String beanName) {
+        Topping t = ctx.getBean(beanName, Topping.class);
+        assertTrue(t.getPrice() > 0, "Il prezzo del topping " + beanName + " deve essere maggiore di zero");
+    }
+
+    //
+    // totale ordine con coperto e pizza extra
+    @Test
+    void testCalcoloTotaleOrdine() {
+        Tavolo t = new Tavolo(1, 4, StatoTavolo.LIBERO);
+        Ordine o = new Ordine();
+        o.setTavolo(t);
+        o.setNumeroCoperti(2);
+        o.setCostoCoperto(2.0);
+
+        Pizza p = new Pizza("Test", 100, 5.0, null);
+        o.getElementiOrdine().add(p);
+
+        assertEquals(9.0, o.getTotale(), "Il totale (5€ pizza + 4€ coperti) deve essere 9.0€");
+    }
+}
